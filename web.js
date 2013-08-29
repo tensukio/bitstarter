70e83716bb61c0710b8d7f49425f8e0c3f18c577
tensukio <tensukio@gmail.com>
2013-08-29 09:51:47 +0000

    Fix #5

--- web.js
+++ web.js
@@ -1,20 +1,22 @@
 var express = require('express');
-
+var app = express();
 var fs = require('fs');
 
 
-var data = new buffer(fs.readFileSync("index.html"));
+data = fs.readFileSync("index.html");
 
-var app = express.createServer(express.logger());
 app.use(express.logger());
 
-data = fs.readFileSync(buffer.toString("data"));
-
 app.get('/', function(request, response) {
-  response.send(data.toString());
+
+var buffer = new Buffer(data);
+response.send(buffer.toString());
+  response.send('Hello Kayla!');
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+    
 });
