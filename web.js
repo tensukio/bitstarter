74ea4b732bee4c053725912af5d0abc599450def
tensukio <tensukio@gmail.com>
2013-08-29 10:01:20 +0000

    completed homework 3 pt 1

--- web.js
+++ web.js
@@ -12,7 +12,7 @@ app.get('/', function(request, response) {
 var buffer = new Buffer(data);
 
 response.write(buffer.toString());
-response.write("Hello Kayla!");
+//response.write("Hello Kayla!");
 response.end();
 
 //response.send(buffer.toString());
